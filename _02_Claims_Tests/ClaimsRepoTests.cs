22fcd90b5d9143fbc1515002c6a46791c9b39df1
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-05-18 14:57:57 -0500

    Built test methods for ClaimsRepository basic CRUD methods

--- _02_Claims_Tests/ClaimsRepoTests.cs
+++ _02_Claims_Tests/ClaimsRepoTests.cs
@@ -1,14 +1,60 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using _02_Claims_Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace _02_Claims_Tests
 {
     [TestClass]
     public class ClaimsRepoTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private ClaimsRepository _repo;
+        private Claim _claim1;
+        private Claim _claim2 = new Claim(
+                2,
+                TypeOfClaim.Home,
+                "Flooded basement",
+                2187637316.00,
+                new DateTime(2021, 02, 01),
+                new DateTime(2021, 03, 01));
+
+        [TestInitialize]
+        public void Arrange()
         {
+            _repo = new ClaimsRepository();
+            _claim1 = new Claim(
+                1,
+                TypeOfClaim.Car,
+                "Crashed into tree at Main St. and 1st St.",
+                8021.23,
+                new DateTime(2021, 01, 01),
+                new DateTime(2021, 02, 01));
+            _repo.AddClaimToQueue(_claim1);
+        }
+
+        [TestMethod]
+        public void AddClaimToQueue_ShouldReturnTrue()
+        {
+            Assert.IsTrue(_repo.AddClaimToQueue(_claim2));
+        }
+
+        [TestMethod]
+        public void ReturnQueue_ShouldReturnCorrectTypeOfQueue()
+        {
+            Assert.IsInstanceOfType(_repo.ReturnQueue(), typeof(Queue<Claim>));
+        }
+
+        [TestMethod]
+        public void UpdateClaimByNumber_ShouldReturnUpdatedValue()
+        {
+            _repo.UpdateClaimByNumber(1, _claim2);
+            Assert.AreEqual(2, _claim2.ClaimID);
+        }
+
+        [TestMethod]
+        public void DequeueNextClaim_ShouldReturnTrue()
+        {
+            Assert.IsTrue(_repo.DequeueNextClaim());
         }
     }
 }
