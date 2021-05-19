ba3c3bc3fe2e1d38615317608a1a17e9e0840fa2
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-05-19 10:20:09 -0500

    Completed BadgesRepo CRUD methods

--- _03_Badges_Repository/BadgesRepository.cs
+++ _03_Badges_Repository/BadgesRepository.cs
@@ -8,6 +8,39 @@ namespace _03_Badges_Repository
 {
     public class BadgesRepository
     {
+        Dictionary<int, List<string>> _badgeDic = new Dictionary<int, List<string>>();
 
+        //CRUD METHODS
+        //CREATE
+        public bool AddBadge(Badge badge)
+        {
+            int originalCount = _badgeDic.Count();
+            _badgeDic.Add(badge.BadgeID, badge.DoorAccessList);
+            return _badgeDic.Count() > originalCount;
+        }
+
+        //READ
+        public Dictionary<int, List<string>> ReturnDictionary()
+        {
+            return _badgeDic;
+        }
+
+        //UPDATE
+        public void UpdateBadgeAccess(int numToUpdate, List<string> updatedDoorAccessList)
+        {
+            _badgeDic[numToUpdate] = updatedDoorAccessList;
+        }
+
+        //DELETE
+        public bool RemoveAllAccess(int numToRemove)
+        {
+            _badgeDic[numToRemove] = new List<string>();
+            if (_badgeDic[numToRemove].Count() == 0) return true;
+            else return false;
+        }
+
+
+        //helper methods
+        
     }
 }
